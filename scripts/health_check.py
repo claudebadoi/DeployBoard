c487849ff1fec420b33db263563c1b1ac63b0f8b
Clau <claud.badoi@gmail.com>
2025-06-02 14:12:03 +0300

    Added logging in the python script

--- scripts/health_check.py
+++ scripts/health_check.py
@@ -1,4 +1,14 @@
 import requests  # library for checking https
+import logging
+from datetime import datetime
+
+#functia pentru loggin in fisierul health_check.log
+logging.basicConfig (
+filename='health_check.log',
+format = '%(asctime)s - %(message)s',
+level = logging.INFO
+)
+
 
 
 # functia pentru verificarea URL
@@ -6,11 +16,11 @@ def check_health(url):
     try:
         response = requests.get(url, timeout=5)
         if response.status_code == 200:
-            print(f"{url} este UP ✅")
+            logging.info(f"{url} este UP")
         else:
-            print(f"{url} a răspuns cu codul: {response.status_code} ⚠️")
+            logging.warning(f"{url} a raspuns cu codul: {response.status_code}")
     except requests.RequestException as e:
-        print(f"{url} este DOWN ❌ – Eroare: {e}")
+        logging.error(f"{url} este DOWN – Eroare: {e}")
 
 
 if __name__ == "__main__":
